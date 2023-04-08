c1f65b1e496edf41fb24a6cb562251ae6212ba6b
dastasoft <45243062+dastasoft@users.noreply.github.com>
2023-04-08 01:14:59 +0000

    feat: update city

--- src/components/Bio/BioBase.js
+++ src/components/Bio/BioBase.js
@@ -7,12 +7,12 @@ const BioBase = ({ className }) => {
       <h1>About Me</h1>
       <div>
         <p>
-          Hi, I'm Daniel and I live in Sant Cugat del Vallès (Barcelona),
-          full-stack developer with 10 years of experience in technologies such
-          as JavaScript, React and Java. My ability to work on both front-end
-          and back-end development, paired with my experience in medium and big
-          companies as well as startups, allows me to create dynamic and
-          user-friendly applications and systems.
+          Hi, I'm Daniel and I live in Tokyo, as a full-stack developer with 10
+          years of experience in technologies such as JavaScript, React and
+          Java, I bring a diverse skill set to any project. My ability to work
+          on both front-end and back-end development, paired with my experience
+          in medium and big companies as well as startups, allows me to create
+          dynamic and user-friendly web applications.
         </p>
         <p>
           In addition to my professional work, I am passionate about sharing my
