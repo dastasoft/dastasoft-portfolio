d7a69ba6f6de48cb716e5d3005f88a04dfc3a405
dastasoft <dastasoft@protonmail.com>
2025-05-08 11:01:29 +0900

    fix: avatar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,6 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import Introduction from '../../components/Introduction';
 import Bio from '../../components/Bio';
-import Skills from '../../components/Skills';
 import HighlightProject from '../../components/HighlightProject';
 import Projects from '../../components/Projects';
 import ProfessionalExp from '../../components/ProfessionalExp';
